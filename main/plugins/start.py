267fb3353f79121c8d94205cb483eaa154bf92de
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-13 23:49:52 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -2,6 +2,7 @@ from .. import Drone
 from telethon import events, Button
 from LOCAL.localisation import START_TEXT as st
 from LOCAL.localisation import JPG as file
+from LOCAL.localisation import info_text, spam_notice, help_text, DEV
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
@@ -11,7 +12,7 @@ async def start(event):
                               ])
                       
 
-@Asst.on(events.callbackquery.CallbackQuery(data="plugins"))
+@Drone.on(events.callbackquery.CallbackQuery(data="menu"))
 async def menu(event):
     await event.edit("",
                     buttons=[[
@@ -23,5 +24,18 @@ async def menu(event):
                          [
                          Button.url("DEVELOPER", url=f"{DEV}")]])
     
-                   
-                         
+@Drone.on(events.callbackquery.CallbackQuery(data="info"))
+async def info(event):
+    await event.edit(f'**INFO:**\n\n{info_text}',
+                    buttons=[[
+                         Button.inline("Menu.", data="menu")]])
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="notice"))
+async def notice(event):
+    await event.answer(f'{notice_text}', alert=True)
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="premium"))
+async def premium(event):
+    await event.answer('You can purchase a package of 70Rs(aprx 1$) per month for renting a bot on heroku, hence no restrictions for usage.' alert=True)
+    
+    
