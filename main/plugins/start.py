252d8e9b93cd7b8a4c362ff70183e7a9215c7da7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-13 23:05:25 +0530

    Update and rename empty to start.py

---
+++ main/plugins/start.py
@@ -0,0 +1,9 @@
+from .. import Drone
+from telethon import events, Button
+from LOCAL.localisation import START_TEXT as st
+from LOCAL.localisation import JPG as file
+
+@Drone.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply(f'{st}', file=file)
+    
