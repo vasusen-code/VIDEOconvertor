799ae187375d6f5d2c6fbb8d2a236f6221e117a1
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-13 23:13:31 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -5,5 +5,9 @@ from LOCAL.localisation import JPG as file
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply(f'{st}', file=file)
-    
+    await event.reply(f'{st}', file=file,
+                      buttons=[
+                              [Button.inline("Menu.", data="menu")]
+                              ])
+                      
+
