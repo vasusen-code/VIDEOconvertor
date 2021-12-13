7961d1da46d7fa4591d8d4de9413b8a10c70d8bd
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-13 22:50:26 +0530

    Create localisation.py

---
+++ LOCAL/localisation.py
@@ -0,0 +1,8 @@
+START_TEXT = "Send me any file to begin."
+
+FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+
+CHANNEL_LINK = "https://t.me/DroneBots"
+
+SUPPORT_LINK = "https://t.me/TeamDrone"
+
