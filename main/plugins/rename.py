e92e13f41996ac04a69f1fa0501e7037bd64c44a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-14 23:57:13 +0530

    Update rename.py

--- main/plugins/rename.py
+++ main/plugins/rename.py
@@ -7,10 +7,11 @@ from telethon import events
 from ethon.telefunc import fast_download, fast_upload
 from ethon.pyfunc import video_metadata
 
-async def media_rename(event, media):
+async def media_rename(event, msg):
     Drone = event.client
     DT = time.time()
+    mime = msg.document.mime_type
+    if 'mp4' in mime:
     await Drone.fast_download(media, Drone, event, DT, "**DOWNLOADING:**")
-    
-    
+       
     
