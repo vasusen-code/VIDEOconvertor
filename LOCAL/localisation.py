184158d9691dfd086631084d38de88944720f579
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-14 09:10:51 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -6,3 +6,13 @@ CHANNEL_LINK = "https://t.me/DroneBots"
 
 SUPPORT_LINK = "https://t.me/TeamDrone"
 
+info_text = "This bot is developed by @ChauhanMahesh\n\nThis bot is coded i python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.1"   
+
+help_text = "Send me any video file to start compressing it or any kind of file to rename it.\n\n/compress - negligible loss compression\n/convert - change formats or extract audio of any video\n/rename - rename any file, extension not required\n/trim - cut your videos" 
+
+premium_text = "contact DEV to rent a bot at 70RS(1$) per month for yourself hosted on heroku for no restrictions on usage. 
+
+DEV = "https://t.me/ChauhanMahesh"
+
+spam_notice = "This bot is hosted on heroku, and hence can run just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+
