5d86d515f684c2d332b5752787a4b6ff9e0a0823
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-14 22:50:56 +0530

    Create rename.py

---
+++ main/plugins/rename.py
@@ -0,0 +1,13 @@
+#TG:ChauhanMahesh/DroneBots
+#Github.com/Vasusen-code
+
+import time
+from .. import Drone
+from telethon import events
+from ethon.telefunc import fast_download, fast_upload
+from ethon.pyfunc import video_metadata
+
+async def media_rename(event, media):
+    Drone = event.client
+    await Drone.fast_download()
+    
