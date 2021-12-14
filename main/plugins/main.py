a8fc89ddfc3365543976fc618c466a3d16c9f0f6
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-14 22:38:20 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -7,12 +7,22 @@ from telethon import events, Button
 @Drone.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
 async def compin(event):
     if event.is_private:
-        msg = event.message
         media = event.media
         video = event.document.mime_type
         if media:
             if 'video' in video:
+                await event.reply("📽",
+                            buttons=[
+                                [Button.inline("COMPRESS", data="compress"),
+                                 Button.inline("CONVERT", data="convert")],
+                                [Button.inline("RENAME", data="rename"),
+                                 Button.inline("TRIM", data="trim")]
+                            ])
+                
             else:
+                await event.reply(,
+                            buttons=[  
+                                [Button.inline("RENAME", data="rename"),
         else:
             return
     
