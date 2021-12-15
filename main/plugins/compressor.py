dbff4a04b7a5befda01487059bc0a087f0448a12
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-15 13:16:46 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -10,3 +10,6 @@ from .. import Drone, BOT_UN
 from telethon import events
 from ethon.telefunc import fast_download, fast_upload
 from LOCAL.localisation import SUPPORT_LINK
+from LOCAL.utils import ffmpeg_progress
+from telethon.errors.rpcerrorlist import MessageNotModifiedError
+from telethon.tl.types import DocumentAttributeVideo
