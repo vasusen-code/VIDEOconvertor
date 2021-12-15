1a6456abe7ef8e4b170ac7cf1e84e6aacc533901
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-15 13:18:48 +0530

    Create utils.py

---
+++ LOCAL/utils.py
@@ -0,0 +1,22 @@
+import time
+import math
+
+def time_formatter(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (
+        ((str(weeks) + "w:") if weeks else "")
+        + ((str(days) + "d:") if days else "")
+        + ((str(hours) + "h:") if hours else "")
+        + ((str(minutes) + "m:") if minutes else "")
+        + ((str(seconds) + "s:") if seconds else "")
+    )
+    if tmp.endswith(":"):
+        return tmp[:-1]
+    else:
+        return tmp
