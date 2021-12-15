88ddebd12d3f2cfbb2f3f366ce6720ffe9ef7de6
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-16 01:51:09 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -5,6 +5,7 @@ import subprocess
 import time
 from telethon import events
 from LOCLAL.localisation import SUPPORT_LINK, JPG
+from ethon.telefunc import fast_download, fast_upload
 from ethon.pyfunc import bash
 
 async def mp3(event, msg):
@@ -25,5 +26,17 @@ async def mp3(event, msg):
         name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
     else:
         name = msg.file.name
-        ext = (name.split("."))[1]
-        
+    try:
+        await fast_download()
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})')
+    try:
+        bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3')
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})')
+    await fast_upload()
+    await Drone.send_file()
+                               
+             
