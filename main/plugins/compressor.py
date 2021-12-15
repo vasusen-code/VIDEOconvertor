563ad02ee0894335c1e93d5dec73ef16b4eb8c06
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-15 13:12:47 +0530

    Create compressor.py

---
+++ main/plugins/compressor.py
@@ -0,0 +1,12 @@
+#TG:ChauhanMahesh/DroneBots
+#Github.com/vasusen-code
+
+import asyncio
+import time
+import subprocess
+import import re
+from datetime import datetime as dt
+from .. import Drone, BOT_UN
+from telethon import events
+from ethon.telefunc import fast_download, fast_upload
+from LOCAL.localisation import SUPPORT_LINK
