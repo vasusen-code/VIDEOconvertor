fad95c39a95778a6e403212aa4a8a287064d9449
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-15 18:02:04 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -10,6 +10,40 @@ from .. import Drone, BOT_UN
 from telethon import events
 from ethon.telefunc import fast_download, fast_upload
 from LOCAL.localisation import SUPPORT_LINK
-#from LOCAL.utils import ffmpeg_progress
+from LOCAL.utils import ffmpeg_progress
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from telethon.tl.types import DocumentAttributeVideo
+
+async def compress(event, msg):
+    Drone = event.client
+    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
+    new_name = "out_" + dt.now().isoformat("_", "seconds")
+    mime = msg.file.mime_type
+    if 'mp4' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        out = new_name + ".mp4"
+    elif msg.video:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        out = new_name + ".mp4"
+    elif 'x-matroska' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+        out = new_name + ".mkv"            
+    elif 'webm' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
+        out = new_name + ".webm"
+    else:
+        name = msg.file.name
+        ext = (name.split("."))[1]
+        out = new_name + ext
+    DT = time.time()
+    await fast_download()
+    cmd
+    await ffmpeg_progress(cmd, edit, '**COMPRESSING:**')
+    UT = time.time()
+    uploader = await fast_upload()
+    await Drone.send_file(event.chat_id, uploader, captio=text, thumb=JPG2, attributes=attributes, force_document=False)
+    await edit.delete()
+    os.remove(name)
+    os.remove(out)
+    
+    
