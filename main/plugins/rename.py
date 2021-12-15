d92a34de4b42e1e9f404ae4db913fe2eba829fc9
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-15 13:00:31 +0530

    Update rename.py

--- main/plugins/rename.py
+++ main/plugins/rename.py
@@ -106,7 +106,7 @@ async def media_rename(event, msg, new_name):
                 width = metadata["width"]
                 height = metadata["height"]
                 duration = metadata["duration"]
-                attributes = [DocumentAttributeVideo(duration=int(duration), w=width, h=height, supports_streaming=True)]
+                attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
                 UT = time.time()
                 uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
                 net_time = round(DT - UT)
