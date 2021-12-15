b88d1b41372e60f7c6fb0ecee8d2fb9f8176d6cc
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-15 17:43:56 +0530

    Update rename.py

--- main/plugins/rename.py
+++ main/plugins/rename.py
@@ -8,7 +8,7 @@ from telethon import events
 from ethon.telefunc import fast_download, fast_upload
 from ethon.pyutils import rename
 from ethon.pyfunc import video_metadata
-from LOCAL.localisation import SUPPORT_LINK, JPG
+from LOCAL.localisation import SUPPORT_LINK, JPG2
 from telethon.tl.types import DocumentAttributeVideo
 
 async def media_rename(event, msg, new_name):
