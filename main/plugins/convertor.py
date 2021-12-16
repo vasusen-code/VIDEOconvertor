c796b97f90b960278a853af300dde4c3fcdf347c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-16 09:15:10 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -1,8 +1,10 @@
 #TG:ChauhanMahesh/DroneBots
 #Github.com/Vasusen-code
 
+import os
 import subprocess
 import time
+from .. import BOT_UN
 from telethon import events
 from LOCLAL.localisation import SUPPORT_LINK, JPG
 from ethon.telefunc import fast_download, fast_upload
@@ -26,17 +28,29 @@ async def mp3(event, msg):
         name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
     else:
         name = msg.file.name
+    out = dt.now().isoformat("_", "seconds")
     try:
-        await fast_download()
+        DT = time.time()
+        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})')
     try:
+        await edit.edit("Converting.")
         bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3')
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})')
-    await fast_upload()
-    await Drone.send_file()
+    try:
+        UT = time.time()
+        uploader = await fast_upload(f'{out}.mp3', f'{out}.mp3', UT, Drone, edit, '**UPLOADING:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : {BOT_UN}', force_document=True)
+   except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})')
+    await edit.delete()
+    os.remove(name)
+    os.remove(out)                           
+                       
                                
              
