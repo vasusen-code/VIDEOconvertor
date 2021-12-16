bbf12c7ac0c759643de91b6a736990ede87cfa8d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-16 10:16:07 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -45,7 +45,7 @@ async def mp3(event, msg):
         UT = time.time()
         uploader = await fast_upload(f'{out}.mp3', f'{out}.mp3', UT, Drone, edit, '**UPLOADING:**')
         await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : {BOT_UN}', force_document=True)
-   except Exception as e:
+    except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})')
     await edit.delete()
