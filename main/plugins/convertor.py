67f8f9c70e0a0fbcfd96e296d0505c0dcf0150c9
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-16 15:00:30 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -106,7 +106,55 @@ async def flac(event, msg):
     os.remove(name)
     os.remove(f'{out}.mp3')                           
     os.remove(f'{out}.flac')                 
-                               
+
+async def wav(event, msg):
+    Drone = event.client
+    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    if hasattr(msg.media, "document"):
+        file = msg.media.document
+    else:
+        file = msg.media
+    x = msg.file.name
+    mime = msg.file.mime_type
+    if x:
+        name = msg.file.name
+    elif 'mp4' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif msg.video:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif 'x-matroska' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+    elif 'webm' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
+    if x:
+        out = ((msg.file.name).split("."))[0]
+    else:
+        out = dt.now().isoformat("_", "seconds")
+    try:
+        DT = time.time()
+        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    try:
+        await edit.edit("Converting.")
+        bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3")
+        bash(f'ffmpeg -i {out}.mp3 {out}.wav')
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    try:
+        UT = time.time()
+        uploader = await fast_upload(f'{out}.wav', f'{out}.wav', UT, Drone, edit, '**UPLOADING:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : @{BOT_UN}', force_document=True)
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    await edit.delete()
+    os.remove(name)
+    os.remove(f'{out}.mp3')                           
+    os.remove(f'{out}.wav')                 
+                                       
 async def mp4(event, msg):
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
@@ -280,7 +328,7 @@ async def file(event, msg):
     await edit.delete()
     os.remove(name)                           
     
-async def file(event, msg):
+async def video(event, msg):
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
     if hasattr(msg.media, "document"):
