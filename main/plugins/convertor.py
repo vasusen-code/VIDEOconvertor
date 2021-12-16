0ae440d2930b1bf6d6fc5150377ad817af7db372
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-16 10:26:56 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -52,5 +52,50 @@ async def mp3(event, msg):
     os.remove(name)
     os.remove(out)                           
                        
+async def flac(event, msg):
+    Drone = event.client
+    edit = await Drone.send_message(even.chat_id, "Trying to process!", reply_to=msg.id)
+    if hasattr(msg.media, "document"):
+        file = msg.media.document
+    else:
+        file = msg.media
+    mime = msg.file.mime_type
+    if 'mp4' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif msg.video:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif 'x-matroska' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+    elif 'webm' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
+    else:
+        name = msg.file.name
+    out = dt.now().isoformat("_", "seconds")
+    try:
+        DT = time.time()
+        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})')
+    try:
+        await edit.edit("Converting.")
+        bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3")
+        bash(f'ffmpeg -i {out}.mp3 -c:a flac {out}.flac')
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})')
+    try:
+        UT = time.time()
+        uploader = await fast_upload(f'{out}.flac', f'{out}.mp3', UT, Drone, edit, '**UPLOADING:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : {BOT_UN}', force_document=True)
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})')
+    await edit.delete()
+    os.remove(name)
+    os.remove(f'{out}.mp3')                           
+    os.remove(f'{out}.flac')                 
                                
+             
+                      
              
