9300503475e9eb16e80cb5691ffbe0fc2e273aa3
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-16 14:52:15 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -6,11 +6,13 @@ import subprocess
 import time
 from .. import BOT_UN
 from telethon import events
-from LOCAL.localisation import SUPPORT_LINK, JPG
+from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2
 from ethon.telefunc import fast_download, fast_upload
-from ethon.pyfunc import bash
+from ethon.pyfunc import bash, video_metadata
 from ethon.pyutils import rename
 from datetime import datetime as dt
+from telethon.tl.types import DocumentAttributeVideo
+
 async def mp3(event, msg):
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
@@ -49,7 +51,7 @@ async def mp3(event, msg):
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.mp3', f'{out}.mp3', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : {BOT_UN}', force_document=True)
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : @{BOT_UN}', force_document=True)
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
@@ -96,7 +98,7 @@ async def flac(event, msg):
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.flac', f'{out}.flac', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : {BOT_UN}', force_document=True)
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : @{BOT_UN}', force_document=True)
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
@@ -143,7 +145,7 @@ async def mp4(event, msg):
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.mp4', f'{out}.mp4', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : {BOT_UN}', force_document=True)
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
@@ -189,7 +191,7 @@ async def mkv(event, msg):
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : @{BOT_UN}', force_document=True)
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
@@ -235,7 +237,7 @@ async def webm(event, msg):
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**AUDIO EXTRACTED by** : @{BOT_UN}', force_document=True)
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
@@ -243,3 +245,78 @@ async def webm(event, msg):
     os.remove(name)                           
     os.remove(f'{out}')
              
+async def file(event, msg):
+    Drone = event.client
+    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    if hasattr(msg.media, "document"):
+        file = msg.media.document
+    else:
+        file = msg.media
+    x = msg.file.name
+    mime = msg.file.mime_type
+    if x:
+        name = msg.file.name
+    elif 'mp4' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif msg.video:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif 'x-matroska' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+    elif 'webm' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
+    try:
+        DT = time.time()
+        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    try:
+        UT = time.time()
+        uploader = await fast_upload(f'{name}', f'{name}', UT, Drone, edit, '**UPLOADING:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    await edit.delete()
+    os.remove(name)                           
+    
+async def file(event, msg):
+    Drone = event.client
+    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    if hasattr(msg.media, "document"):
+        file = msg.media.document
+    else:
+        file = msg.media
+    x = msg.file.name
+    mime = msg.file.mime_type
+    if x:
+        name = msg.file.name
+    elif 'mp4' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif msg.video:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+    elif 'x-matroska' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+    elif 'webm' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
+    try:
+        DT = time.time()
+        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    try:
+        metadata = video_metadata(out)
+        width = metadata["width"]
+        height = metadata["height"]
+        duration = metadata["duration"]
+        attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]           
+        UT = time.time()
+        uploader = await fast_upload(f'{name}', f'{name}', UT, Drone, edit, '**UPLOADING:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG2, caption=f'**CONVERTED by** : @{BOT_UN}', attributes=attributes, force_document=False)
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+    await edit.delete()
+    os.remove(name)                           
+    
