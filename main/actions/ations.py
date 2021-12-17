3f3d85dac47cdd50ceb5d8191bf8a342e21b9ab6
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-17 12:28:43 +0530

    Update ations.py

--- main/actions/ations.py
+++ main/actions/ations.py
@@ -1,7 +1,49 @@
-from .. import Drone, AUTH_USERS, LOG_CHANNRL, ACCESS_CHANNEL
+from .. import Drone, AUTH_USERS, ACCESS_CHANNEL
 from telethon import events 
 import pymongo
 from pymongo import MongoClient
 import motor.motor_asyncio
 from TelethonBot.Database.mongodb import Database
 
+db = Database(MONGODB_URI, SESSION_NAME)
+
+@Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+async def incomming(event):
+    if not await db.is_user_exist(event.sender_id):
+        await db.add_user(event.sender_id)
+    await event.forward_to(int(ACCESS_CHANNEL))
+
+async def banned(id):
+    await db.get_ban_status(id)
+
+@Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="/users"))
+async def listusers(event):
+    xx = await event.reply("Counting total users on db...")
+    x = await db.total_users_count()
+    await xx.edit(f"Total user(s) {int(x)}")
+    
+@Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="^/disallow (.*)" ))
+async def bban(event):
+    c = event.pattern_match.group(1)
+    if not c:
+        await event.reply("Disallow who!?")
+    if c in AUTH_USERS:
+        return await event.reply("I cannot ban an AUTH_USER")
+    xx = await db.is_banned(event.sender_id)
+    if xx is True:
+        return await event.reply("User is already disallowed!")
+    else:
+        await db.banning(c)
+        await event.reply(f"{c} is now disallowed.")
+        
+@Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="^/allow (.*)" ))
+async def unbban(event):
+    xx = event.pattern_match.group(1)
+    xy = await db.is_banned(xx)
+    if xy is False:
+        return await event.reply("User is already allowed!")
+    if not xx:
+        await event.reply("Allow who?")
+    await db.unbanning(xx)
+    await event.reply(f"{xx} Allowed! ")
+    
