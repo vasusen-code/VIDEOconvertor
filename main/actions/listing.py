8e762552311e64abddf092bb8660acf4634492c8
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-17 15:37:20 +0530

    Create listing.py

---
+++ main/actions/listing.py
@@ -0,0 +1,19 @@
+#Tg:ChauhanMahesh/DroneBots
+#Github.com/vasusen-code
+
+def queue(id, List1, List2):
+    if not f'{id}' in List1:
+        List1.append(f'{id}')
+    else:
+        if not f'{id}' in List2:
+            List2.append(f'{id}')
+        else:
+            return False
+          
+def ps_queue(id, media, List1, List2):
+    List1.append(f'{id}')
+    List2.append(media)
+    if not len(List1) < 2:
+        return 'EMPTY'
+    if len(List1) > 2:
+        return 'FULL'
