3434066b9223bda339256d2f0beaf43165c284d4
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-19 00:07:16 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -22,6 +22,6 @@ JPG2 = "LOCAL/20211215_165751.jpg"
 
 JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
 
-JPG4 = "LOCAL/20211217_000535.jpg"
+JPG4 = "LOCAL/20211219_000258.jpg"
 
 forcesubtext = "Hey there!To use this bot you've to join @DroneBots.\n\nIf you're banned , then make a request for unban in @TeamDrone."
