93b3ef945fe84c819c354a37c049917167280ef5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-18 15:17:07 +0530

    Update __.py

--- main/plugins/__.py
+++ main/plugins/__.py
@@ -59,7 +59,7 @@ async def unbban(event):
 
 async def LOG_START(event, ps_name):
     chat = config("LOG_CHANNEL", default=None)
-    Tag = mention(event.client, event.sender_id)
+    Tag = await mention(event.client, event.sender_id)
     xx = await event.client.send_message(chat, f'{ps_name} by {Tag}')
     return xx
 
