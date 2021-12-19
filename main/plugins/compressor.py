d3e3e036455aa7d75452ff4cfb6136f0c3f3a4bb
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-19 09:13:35 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -92,3 +92,40 @@ async def compress(event, msg, List1):
     os.remove(out)
     await log.delete()
     await LOG_END(event, log_end_text)
+    
+async def file_compress(event, name, List1):
+    log = await LOG_START(event, f'**COMPRESS PROCESS STARTED**\n\n[Bot is busy now]({SUPPORT_LINK})')
+    log_end_text = f'**COMPRESS PROCESS FINISHED**\n\n[Bot is free now]({SUPPORT_LINK})'
+    edit = event
+    out = "out_" + dt.now().isoformat("_", "seconds") + (name.split("."))[1]
+    FT = time.time()
+    progress = f"progress-{FT}.txt"
+    cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 28 -acodec copy """{out}""" -y'
+    try:
+        await ffmpeg_progress(cmd, name, progress, FT, edit, '**COMPRESSING:**')
+    except Exception as e:
+        await log.delete()
+        await LOG_END(event, log_end_text)
+        os.rmdir("compressmedia")
+        print(e)
+        return await edit.edit(f"An error occured while FFMPEG progress.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)   
+    i_size = os.path.getsize(name)
+    f_size = os.path.getsize(out)
+    text = f'**COMPRESSED by** : @{BOT_UN}\n\nbefore compressing : `{i_size}`\nafter compressing : `{f_size}`'
+    UT = time.time()
+    try:
+        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
+        await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+    except Exception:
+        await log.delete()
+        await LOG_END(event, log_end_text)
+        os.rmdir("compressmedia")
+        print(e)
+        return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    await edit.delete()
+    List1.append(f'{event.sender_id}')
+    os.remove(name)
+    os.remove(out)
+    await log.delete()
+    await LOG_END(event, log_end_text)
+    
