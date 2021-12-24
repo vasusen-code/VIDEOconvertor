1908933dba674bba90a251938d0f0a3d2115620a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-25 00:00:20 +0530

    Update __.py

--- main/plugins/__.py
+++ main/plugins/__.py
@@ -77,11 +77,11 @@ async def unbban(event):
 async def LOG_START(event, ps_name):
     Tag = mention(event.sender.first_name, event.sender_id)
     text = f'{ps_name}\n\nUSER: {Tag}'
-    xx = await event.client.send_message(int(LOG_ID), text)
+    xx = await event.client.send_message(int(LOG_ID), text, link_preview=False)
     return xx
 
 async def LOG_END(event, ps_name):
-    await event.client.send_message(int(LOG_ID), f'{ps_name}')
+    await event.client.send_message(int(LOG_ID), f'{ps_name}', link_preview=False)
 
 @Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS, pattern="^/msg (.*)"))
 async def msg(event):
