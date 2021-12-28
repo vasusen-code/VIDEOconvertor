ae91b5a56b95823936095c08c55cdd871772c338
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-29 00:43:20 +0530

    Update database.py

--- main/Database/database.py
+++ main/Database/database.py
@@ -8,15 +8,17 @@ SESSION_NAME = 'videoconvertor'
 
 class Database:
   
+#Connection--------------------------------------------------------------------
+
     def __init__(self, MONGODB_URI, SESSION_NAME):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
         self.db = self._client[SESSION_NAME]
         self.col = self.db.users
-        self.ban = self.db.niggas
-        
+
+ #collection handling---------------------------------------------------------
+
     def new_user(self, id):
-        DT = datetime.date.max.isoformat()
-        return dict(id=id , limit=dict(limited_on=DT))
+        return dict(id=id, banned=False, link=None)
            
     async def add_user(self,id):
         user = self.new_user(id)
@@ -30,32 +32,29 @@ class Database:
         count = await self.col.count_documents({})
         return count
 
-    def ban_user(self, id):
-        return dict(banned=id)
-      
     async def banning(self, id):
-        ban = self.ban_user(id)
-        await self.ban.insert_one(ban)
-        
+        await self.col.update_one({'id': id}, {'$set': {'banned': True}})
+    
     async def is_banned(self, id):
-        banned = await self.ban.find_one({'banned': int(id)})
-        return True if banned else False
+        user = await self.col.find_one({'id': int(id)})
+        banned = user.get('banned', False)
+        return banned
       
     async def unbanning(self, id):
-        await self.ban.delete_many({'banned': int(id)})
-    
+        await self.col.update_one({'id': id}, {'$set': {'banned': False}})
+        
     async def get_users(self):
         users = self.col.find({})
         return users
     
-    async def limit_user(self, id):
-        DT = datetime.date.today().isoformat()
-        limit = dict(limited_on=DT)
-        await self.col.update_one({'id': id}, {'$set': {'limit': limit}})
+    async def update_thumb_link(self, id, link):
+        await self.col.update_one({'id': id}, {'$set': {'link': link}})
     
-    async def get_limit(self, id):
+    async def rem_thumb_link(self, id):
+        await self.col.update_one({'id': id}, {'$set': {'link': None}})
+        
+    async def get_thumb(self, id):
         user = await self.col.find_one({'id':int(id)})
-        DT = datetime.date.max.isoformat()
-        default = dict(limited_on=DT)
-        return user.get('limit', default)
+        return user.get('link', None)
+   
     
