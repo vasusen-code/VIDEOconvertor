c30ce76d9b1dab8769bbff2bb8f3d979e99a576c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-30 16:44:55 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,34 +1,53 @@
-from .. import Drone
+#tg:ChauhanMahesh/DroneBots
+#github.com/vasusen-code
+
+from .. import Drone, ACCESS_CHANNEL
 from telethon import events, Button
 from LOCAL.localisation import START_TEXT as st
 from LOCAL.localisation import JPG as file
+from LOCAL.localisation import JPG4
 from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
+from ethon.teleutils import mention
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply(f'{st}', file=file,
+    await event.reply(f'{st}', 
                       buttons=[
                               [Button.inline("Menu.", data="menu")]
                               ])
-                      
-
+    tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
+    await Drone.send_message(int(ACCESS_CHANNEL), f'{tag} started the BOT')
+    
 @Drone.on(events.callbackquery.CallbackQuery(data="menu"))
 async def menu(event):
+    await event.client.send_file(event.chat_id, caption="**📑MENU.**", file=file,
+                    buttons=[[
+                         Button.inline("info.", data="info"),
+                         Button.inline("SOURCE-CODE", data="source")],
+                         [
+                         Button.inline("NOTICE.", data="notice"),
+                         Button.inline("Help/SETTINGS.", data="help")],
+                         [
+                         Button.url("DEVELOPER", url=f"{DEV}")]])
+    await event.delete()
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="menu2"))
+async def menu2(event):
     await event.edit("**📑MENU.**",
                     buttons=[[
                          Button.inline("info.", data="info"),
-                         Button.inline("NOTICE", data="notice")],
+                         Button.inline("SOURCE-CODE", data="source")],
                          [
-                         Button.inline("SOURCE", data="source"),
-                         Button.inline("Help.", data="help")],
+                         Button.inline("NOTICE.", data="notice"),
+                         Button.inline("Help/SETTINGS.", data="help")],
                          [
                          Button.url("DEVELOPER", url=f"{DEV}")]])
-    
+       
 @Drone.on(events.callbackquery.CallbackQuery(data="info"))
 async def info(event):
     await event.edit(f'**ℹ️NFO:**\n\n{info_text}',
                     buttons=[[
-                         Button.inline("Menu.", data="menu")]])
+                         Button.inline("Menu.", data="menu2")]])
     
 @Drone.on(events.callbackquery.CallbackQuery(data="notice"))
 async def notice(event):
@@ -36,27 +55,56 @@ async def notice(event):
     
 @Drone.on(events.callbackquery.CallbackQuery(data="source"))
 async def source(event):
-    await Drone.send_message(event.chat_id, source_text,
+    await event.edit(source_text,
                     buttons=[[
-                         Button.url("GITHUB.", url="https://github.com/Vasusen-code/VideoConvertor/tree/public")]])
-
-    
+                         Button.url("Main.", url="https://github.com/vasusen-code/videoconvertor/tree/main"),
+                         Button.url("PUBLIC", url="https://github.com/vasusen-code/videoconvertor/tree/public")]])
+                         
+                    
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
 async def help(event):
-    await event.edit('**👥HELP.**',
+    await event.edit('**👥HELP & SETTINGS.**',
                     buttons=[[
-                         Button.inline("PLUGINS.", data="plugins"),
+                         Button.inline("set THUMBNAIL.", data="sett"),
+                         Button.inline("rem THUMBNAIL.", data='remt')],
+                         [
+                         Button.inline("PLUGUNS..", data="plugins"),
                          Button.url("SUPPORT.", url=f"{SUPPORT_LINK}")],
                          [
-                         Button.inline("Menu.", data="menu")]])
+                         Button.inline("Menu.", data="menu2")]])
     
 @Drone.on(events.callbackquery.CallbackQuery(data="plugins"))
 async def plugins(event):
     await event.edit(f'{help_text}',
                     buttons=[[
-                         Button.inline("Menu.", data="menu")]])
+                         Button.inline("Menu.", data="menu2")]])
     
-    
-    
+@Drone.on(events.callbackquery.CallbackQuery(data="sett"))
+async def sett(event):    
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        x = await conv.get_reply()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found.")
+        await xx.delete()
+        t = await event.client.send_message(event.chat_id, 'Trying.')
+        path = await event.client.download_media(x.media)
+        os.rename(path, f'{event.sender_id}.jpg')
+        os.remove(path)
+        await t.edit("Temporary thumbnail saved!")
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="remt"))
+async def remt(event):  
+    await event.edit('Trying.')
+    await os.remove(f'{event.sender_id}.jpg')
+    await event.edit('Removed!')
     
     
