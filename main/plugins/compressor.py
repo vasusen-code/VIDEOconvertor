fc1d24c9c65454c2d2a43b2edf96cbdb397b535f
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-30 21:02:08 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -65,22 +65,20 @@ async def compress(event, msg):
         await LOG_END(event, log_end_text)
         os.rmdir("compressmedia")
         print(e)
-        return await edit.edit(f"An error occured while FFMPEG progress.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)   
+        return await edit.edit(f"An error occured while FFMPEG progress.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)  
+    out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
+    if msg.file.name:
+        out2 = msg.file.name
+    else:
+        out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
+    os.rename(out, out2)
     i_size = os.path.getsize(name)
-    f_size = os.path.getsize(out)
+    f_size = os.path.getsize(out2)     
     text = f'**COMPRESSED by** : @{BOT_UN}\n\nbefore compressing : `{i_size}`\nafter compressing : `{f_size}`'
     UT = time.time()
-    metadata = video_metadata(out)
-    width = metadata["width"]
-    height = metadata["height"]
-    duration = metadata["duration"]
-    attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
-    try:
-        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
-    except Exception:
+    if 'x-matroska' in mime:
         try:
-            uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
         except Exception as e:
             await log.delete()
@@ -88,9 +86,38 @@ async def compress(event, msg):
             os.rmdir("compressmedia")
             print(e)
             return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    elif 'webm' in mime:
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+        except Exception as e:
+            await log.delete()
+            await LOG_END(event, log_end_text)
+            os.rmdir("compressmedia")
+            print(e)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    else:
+        metadata = video_metadata(out)
+        width = metadata["width"]
+        height = metadata["height"]
+        duration = metadata["duration"]
+        attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
+       except Exception:
+           try:
+               uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+               await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+          except Exception as e:
+              await log.delete()
+              await LOG_END(event, log_end_text)
+              os.rmdir("compressmedia")
+              print(e)
+              return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
     os.remove(name)
-    os.remove(out)
+    os.remove(out2)
     await log.delete()
     log_end_text2 = f'**COMPRESS PROCESS FINISHED**\n\nTime Taken: {round((time.time()-DT)/60)} minutes\nInitial size: {i_size/1000000}mb.\nFinal size: {f_size/1000000}mb.\n\n[Bot is free now.]({SUPPORT_LINK})'
     await LOG_END(event, log_end_text2)
