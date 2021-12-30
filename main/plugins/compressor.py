19cbd0cb326964b06dc79ce5f3b5a681debec6e2
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-30 21:10:20 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -105,16 +105,16 @@ async def compress(event, msg):
         try:
             uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
-       except Exception:
-           try:
-               uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
-               await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
-           except Exception as e:
-               await log.delete()
-               await LOG_END(event, log_end_text)
-               os.rmdir("compressmedia")
-               print(e)
-               return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        except Exception:
+            try:
+                uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+                await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+            except Exception as e:
+                await log.delete()
+                await LOG_END(event, log_end_text)
+                os.rmdir("compressmedia")
+                print(e)
+                return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
     os.remove(name)
     os.remove(out2)
