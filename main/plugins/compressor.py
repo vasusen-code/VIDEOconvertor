7c1fc76fd7fc7c1eca677f7605e02af62a765b90
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-30 20:49:46 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -26,20 +26,20 @@ async def compress(event, msg):
         file = msg.media
     mime = msg.file.mime_type
     if 'mp4' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
         out = new_name + ".mp4"
     elif msg.video:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
         out = new_name + ".mp4"
     elif 'x-matroska' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
-        out = new_name + ".mkv"            
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+        out = new_name + ".mp4"            
     elif 'webm' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
-        out = new_name + ".webm"
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
+        out = new_name + ".mp4"
     else:
-        name = msg.file.name
-        ext = (name.split("."))[1]
+        n = msg.file.name
+        ext = (n.split("."))[1]
         out = new_name + ext
     DT = time.time()
     try:
@@ -48,6 +48,8 @@ async def compress(event, msg):
         os.rmdir("compressmedia")
         print(e)
         return await edit.edit(f"An error occured while downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
+    name =  '__' + dt.now().isoformat("_", "seconds") + ".mp4"
+    os.rename(n, name)
     FT = time.time()
     progress = f"progress-{FT}.txt"
     cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 28 -acodec copy """{out}""" -y'
@@ -57,21 +59,43 @@ async def compress(event, msg):
         os.rmdir("compressmedia")
         print(e)
         return await edit.edit(f"An error occured while FFMPEG progress.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)   
+    out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
+    if msg.file.name:
+        out2 = msg.file.name
+    else:
+        out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
+    os.rename(out, out2)
     i_size = os.path.getsize(name)
-    f_size = os.path.getsize(out)
+    f_size = os.path.getsize(out2)
     text = f'**COMPRESSED by** : @{BOT_UN}\n\nbefore compressing : `{i_size}`\nafter compressing : `{f_size}`'
     UT = time.time()
-    metadata = video_metadata(out)
+    if 'webm' in mime:
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+        except Exception as e:
+            os.rmdir("compressmedia")
+            print(e)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    if 'x-matroska' in mime:
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+        except Exception as e:
+            os.rmdir("compressmedia")
+            print(e)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    metadata = video_metadata(out2)
     width = metadata["width"]
     height = metadata["height"]
     duration = metadata["duration"]
     attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
     try:
-        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
+        uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
         await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
     except Exception:
         try:
-            uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
         except Exception as e:
             os.rmdir("compressmedia")
@@ -79,5 +103,5 @@ async def compress(event, msg):
             return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
     os.remove(name)
-    os.remove(out)
+    os.remove(out2)
     
