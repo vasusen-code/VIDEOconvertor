16fdd883583afd6cf44871fd5bd55c3fdf018baf
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-31 09:21:08 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -18,6 +18,8 @@ spam_notice = "This bot is hosted on heroku, and hence can run just run one proc
 
 JPG = "LOCAL/video_convertor.jpg"
 
+JPG0 = 'https://telegra.ph/VC-12-31'
+
 JPG2 = "LOCAL/20211215_165751.jpg"
 
 JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
