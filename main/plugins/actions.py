422b2ee3adbb5164a5cc293c6941116069c9be1d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-06 14:05:23 +0530

    Update actions.py

--- main/plugins/actions.py
+++ main/plugins/actions.py
@@ -1,4 +1,3 @@
-
 #tg:chauhanMahesh/DroneBots
 #github.com/vasusen-code
 
@@ -73,4 +72,61 @@ async def heroku_restart():
             print(e)
             x = False
     return x
+
+#Logging events on tg---------------------------------------------------------------------------------------------
+
+async def LOG_START(event, ps_name):
+    LOG_ID = config("LOG_ID", default=None)
+    chat = LOG_ID
+    if not str(LOG_ID).startswith("-100"):
+        chat = int("-100" + str(LOG_ID))
+    Tag = mention(event.sender.first_name, event.sender_id)
+    text = f'{ps_name}\n\nUSER: {Tag}'
+    xx = await event.client.send_message(int(chat), text, link_preview=False)
+    return xx
+
+async def LOG_END(event, ps_name):
+    LOG_ID = config("LOG_ID", default=None)
+    chat = LOG_ID
+    if not str(LOG_ID).startswith("-100"):
+        chat = int("-100" + str(LOG_ID))
+    await event.client.send_message(int(chat), f'{ps_name}', link_preview=False)
+
+@Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS, pattern="^/msg (.*)"))
+async def msg(event):
+    ok = await event.get_reply_message()
+    if not ok:
+        await event.reply("Reply to the message you want to send!")
+    user = event.pattern_match.group(1)
+    if not user:
+        await event.reply("Give the user id you want me to send message. ")
+    await Drone.send_message(int(user) , ok )
+    await event.reply("Messsage sent.")
+    
+#Listing--------------------------------------------------------------------------------------------------------------
+
+#Not in use
+def one_trial_queue(id, List1):
+    if f'{id}' in List1:
+        return False
+    
+#Not in use
+def two_trial_queue(id, List1, List2):
+    if not f'{id}' in List1:
+        List1.append(f'{id}')
+    else:
+        if not f'{id}' in List2:
+            List2.append(f'{id}')
+        else:
+            return False
+
+#Not in use        
+def ps_queue(id, media, List1, List2):
+    List1.append(f'{id}')
+    List2.append(media)
+    if not len(List1) < 2:
+        return 'EMPTY'
+    if len(List1) > 2:
+        return 'FULL'
+
     
