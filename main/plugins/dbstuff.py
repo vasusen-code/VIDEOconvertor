ad941084c810ab8cb310e720329d6a5a19d5eeb7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-06 14:03:24 +0530

    Update dbstuff.py

--- main/plugins/dbstuff.py
+++ main/plugins/dbstuff.py
@@ -1,17 +1,10 @@
 #tg:chauhanMahesh/DroneBots
 #github.com/vasusen-code
-
-import heroku3 
-from .. import Drone, AUTH_USERS, ACCESS_CHANNEL, MONGODB_URI
-from telethon import events , Button
+ 
+from .. import Drone, AUTH_USERS, MONGODB_URI
+from telethon import events 
 from decouple import config
 from main.Database.database import Database
-from telethon.errors.rpcerrorlist import UserNotParticipantError
-from telethon.tl.functions.channels import GetParticipantRequest
-from telegraph import upload_file
-from telethon.errors.rpcerrorlist import FloodWaitError
-def mention(name, id):
-    return f'[{name}](tg://user?id={id})'
 
 #Database command handling--------------------------------------------------------------------------
 
@@ -103,61 +96,6 @@ async def unbban(event):
     await db.unbanning(int(xx))
     await event.reply(f"{xx} Allowed! ")
     
-#Logging events on tg---------------------------------------------------------------------------------------------
-
-async def LOG_START(event, ps_name):
-    LOG_ID = config("LOG_ID", default=None)
-    chat = LOG_ID
-    if not str(LOG_ID).startswith("-100"):
-        chat = int("-100" + str(LOG_ID))
-    Tag = mention(event.sender.first_name, event.sender_id)
-    text = f'{ps_name}\n\nUSER: {Tag}'
-    xx = await event.client.send_message(int(chat), text, link_preview=False)
-    return xx
-
-async def LOG_END(event, ps_name):
-    LOG_ID = config("LOG_ID", default=None)
-    chat = LOG_ID
-    if not str(LOG_ID).startswith("-100"):
-        chat = int("-100" + str(LOG_ID))
-    await event.client.send_message(int(chat), f'{ps_name}', link_preview=False)
-
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS, pattern="^/msg (.*)"))
-async def msg(event):
-    ok = await event.get_reply_message()
-    if not ok:
-        await event.reply("Reply to the message you want to send!")
-    user = event.pattern_match.group(1)
-    if not user:
-        await event.reply("Give the user id you want me to send message. ")
-    await Drone.send_message(int(user) , ok )
-    await event.reply("Messsage sent.")
-    
-#Listing--------------------------------------------------------------------------------------------------------------
-
-#Not in use
-def one_trial_queue(id, List1):
-    if f'{id}' in List1:
-        return False
-    
-#Not in use
-def two_trial_queue(id, List1, List2):
-    if not f'{id}' in List1:
-        List1.append(f'{id}')
-    else:
-        if not f'{id}' in List2:
-            List2.append(f'{id}')
-        else:
-            return False
-
-#Not in use        
-def ps_queue(id, media, List1, List2):
-    List1.append(f'{id}')
-    List2.append(media)
-    if not len(List1) < 2:
-        return 'EMPTY'
-    if len(List1) > 2:
-        return 'FULL'
 
     
 
