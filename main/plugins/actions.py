e195a37b12d685e65fc4971449aa287ea3c19cc7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-05 10:53:55 +0530

    Update actions.py

--- main/plugins/actions.py
+++ main/plugins/actions.py
@@ -35,6 +35,7 @@ async def force_sub(id):
 #Thumbnail--------------------------------------------------------------------------------------------------------------
 
 async def set_thumbnail(event, img):
+    db = Database(MONGODB_URI, 'videoconvertor')
     edit = await event.client.send_message(event.chat_id, 'Trying to process.')
     try:
         path = await event.client.download_media(img)
@@ -47,6 +48,7 @@ async def set_thumbnail(event, img):
     await edit.edit("Done!")
     
 async def rem_thumbnail(event):
+    db = Database(MONGODB_URI, 'videoconvertor')
     edit = await event.client.send_message(event.chat_id, 'Trying.')
     T = await db.get_thumb(event.sender_id)
     if T is None:
