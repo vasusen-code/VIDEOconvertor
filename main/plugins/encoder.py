19dd86b066a208270854710264f2922f3a5e3057
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-21 21:46:58 +0530

    Update encoder.py

--- main/plugins/encoder.py
+++ main/plugins/encoder.py
@@ -16,7 +16,7 @@ from LOCAL.utils import ffmpeg_progress
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from telethon.tl.types import DocumentAttributeVideo
 
-async def encode(event, msg, scale):
+async def encode(event, msg, scale=0):
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
     new_name = "out_" + dt.now().isoformat("_", "seconds")
@@ -52,7 +52,15 @@ async def encode(event, msg, scale):
     os.rename(n, name)
     FT = time.time()
     progress = f"progress-{FT}.txt"
-    cmd = f'{"ffmpeg -hide_banner -loglevel quiet -progress " + progress + " -i " + name " -filter:v scale=" + scale + ":-1 -c:a copy " + out + " -y"}'
+    cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -filter:v scale=0:-1 -c:a copy """{out}""" -y'
+    if scale == 240:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -filter:v scale=240:-1 -c:a copy """{out}""" -y'
+    elif scale == 360:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -filter:v scale=360:-1 -c:a copy """{out}""" -y'
+    elif scale == 480:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -filter:v scale=480:-1 -c:a copy """{out}""" -y'
+    elif scale == 720:
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -filter:v scale=720:-1 -c:a copy """{out}""" -y'
     try:
         await ffmpeg_progress(cmd, name, progress, FT, edit, '**ECODING:**')
     except Exception as e:
