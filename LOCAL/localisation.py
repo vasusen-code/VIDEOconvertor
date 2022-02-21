60136f188167c5e45e89d4abc2255377cbb407b0
Selfie SD <statuseditoff@gmail.com>
2022-02-21 20:44:25 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,7 +1,7 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Send me any file to begin.\n What's New? \n\n✪ Encode \n✪ Compressor \n✪ Convert \n✪ Rename \n✪ Screenshot \n✪ Trim \n\n〽️ Powered by @Groupdcbots  "
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
