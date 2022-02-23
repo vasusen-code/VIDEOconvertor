077e0c07c7357bd4e96d534a7d19429da29c1938
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-23 15:12:00 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -4,8 +4,6 @@
 from .. import Drone, ACCESS_CHANNEL, AUTH_USERS
 from telethon import events, Button
 from LOCAL.localisation import START_TEXT as st
-from LOCAL.localisation import JPG0 as file
-from LOCAL.localisation import JPG4
 from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
 from ethon.teleutils import mention
 from ethon.mystarts import vc_menu
