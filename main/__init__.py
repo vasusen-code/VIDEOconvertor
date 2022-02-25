f8b15b4a097968737285e228bd0ba44ce77348a5
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-25 20:08:07 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,16 @@
-#ChauhanMahesh/DroneBots/COL
+#  This file is part of the VIDEOconvertor distribution.
+#  Copyright (c) 2021 vasusen-code ; All rights reserved. 
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
 
 from telethon import TelegramClient
 from decouple import config
