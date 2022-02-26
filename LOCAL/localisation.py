3c5b1855c079385527ffb17f2ea94777ce0a66e1
Nasurancreation <88917315+Nasurancreation@users.noreply.github.com>
2022-02-26 06:57:57 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -2,24 +2,24 @@ START_TEXT = "Send me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://telegram.me/ITSOKTV"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://telegram.me/ITSOKTV"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.3"   
+info_text = "This bot is developed by @Dailydoseyt\n\nWritten in python library TELETHON.\n\nBot by : @ITSOKTV\nSupport : @ITSOKTV\n\nV1.3"   
 
 help_text = "Send me any video file to start compressing it or any kind of file to rename it.\n\ncompress - negligible loss compression\nconvert - change formats or extract audio of any video\nrename - rename any file, extension not required\ntrim - cut your videos" 
 
 source_text = "Deploy your own bot.\n\nMain branch - Personal use\nPublic branch - For your channel"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://telegram.me/ITSOKTV"
 
 spam_notice = "This bot is hosted on heroku, and hence can run just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
-JPG = "LOCAL/video_convertor.jpg"
+JPG = "LOCAL/IMG-20220223-WA0001.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = "LOCAL/IMG-20220223-WA0001.jpg"
 
-JPG2 = "LOCAL/20211215_165751.jpg"
+JPG2 = "LOCAL/IMG-20220223-WA0001.jpg"
 
-JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
+JPG3 = "LOCAL/IMG-20220223-WA0001.jpg"
