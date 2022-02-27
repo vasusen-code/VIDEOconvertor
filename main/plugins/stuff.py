4790dc7f332c1eecf8d1206ca730ff7b9517f4d1
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-27 16:43:21 +0530

    Update stuff.py

--- main/plugins/stuff.py
+++ main/plugins/stuff.py
@@ -1,1 +1,49 @@
+from datetime import datetime as dt
 
+from telethon import events
+from ethon.pyfunc import fast_upload, fast_download
+
+def dl_name(mime):
+    name = None
+    if 'mp4' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        return name
+    elif msg.video:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        return name
+    elif 'x-matroska' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+        return name            
+    elif 'webm' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
+        return name
+    elif 'zip' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".zip" 
+        return name            
+    elif 'jpg' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".jpg" 
+        return name
+    elif 'png' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".png"
+        return name
+    elif 'pdf' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".pdf" 
+        return name
+    elif 'rar' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".rar"
+        return name
+    elif 'mp3' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp3" 
+        return name
+    elif 'ogg' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".ogg" 
+        return name          
+    elif 'flac' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".flac"  
+        return name
+    elif 'wav' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".wav" 
+        return name
+    elif 'webp' in mime:
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webp" 
+        return name
