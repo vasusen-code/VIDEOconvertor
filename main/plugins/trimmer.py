163b9eeb33e35cc32e29e1f489bc7a60d94d537a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-27 20:58:30 +0530

    Update trimmer.py

--- main/plugins/trimmer.py
+++ main/plugins/trimmer.py
@@ -12,74 +12,42 @@
 #
 #  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
 
-import time
-import os
+import time, os
 from datetime import datetime as dt
+
 from .. import Drone, BOT_UN
+
+from pyrogram import Client
+
 from telethon import events
-from ethon.telefunc import fast_download, fast_upload
+
 from ethon.pyfunc import video_metadata, bash
 from ethon.pyutils import rename
+
 from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
-from telethon.errors.rpcerrorlist import MessageNotModifiedError
-from telethon.tl.types import DocumentAttributeVideo
 
 async def trim(event, msg, st, et):
     Drone = event.client
     edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
-    new_name = "out_" + dt.now().isoformat("_", "seconds")
-    if hasattr(msg.media, "document"):
-        file = msg.media.document
-    else:
-        file = msg.media
-    mime = msg.file.mime_type
-    if 'mp4' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
-        out = new_name + ".mp4"
-    elif msg.video:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
-        out = new_name + ".mp4"
-    elif 'x-matroska' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
-        out = new_name + ".mkv"       
-    elif 'webm' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
-        out = new_name + ".webm"
-    else:
-        name = msg.file.name
-        ext = (name.split("."))[1]
-        out = new_name + ext
     DT = time.time()
     try:
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        name = await download(msg, edit) 
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
     try:
+        out = "trimmed_" + name
         await edit.edit("Trimming.")
-        bash(f'ffmpeg -i {name} -ss {st} -to {et} -acodec copy -vcodec copy {out}')
-        out2 = new_name + '_2_' + '.mp4'
-        rename(out, out2)
+        bash(f'ffmpeg -i {name} -ss {st} -to {et} -acodec copy -vcodec copy {out} -y')
     except Exception as e:
         print(e)
         return await edit.edit(f"An error occured while trimming!\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
-    UT = time.time()
     text = f"**TRIMMED by :** @{BOT_UN}"
     try:
-        metadata = video_metadata(out2)
-        width = metadata["width"]
-        height = metadata["height"]
-        duration = metadata["duration"]
-        attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
-        uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
-    except Exception:
-        try:
-            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
-            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
-        except Exception as e:
-            print(e)
-            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        await upload(out, edit, thumb=JPG, caption=text) 
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
     os.remove(name)
     os.remove(out2)
