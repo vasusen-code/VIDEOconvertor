d7635bf94afb1acdeec46c2a765d6ecf890e6cda
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-03-10 22:14:15 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -16,6 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
+print("@MaheshChauhan • @DroneBots")
 
 if __name__ == "__main__":
     Drone.run_until_disconnected()
