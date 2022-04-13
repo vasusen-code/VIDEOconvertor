6d2bf19b8bf9305f4e6c3b7d6cb56c1d8a0cacc9
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-04-14 00:59:44 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -13,15 +13,18 @@
 #  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
 
 import asyncio, time, subprocess, re, os, ffmpeg
+
 from datetime import datetime as dt
-from .. import Drone, BOT_UN, LOG_CHANNEL
 from telethon import events
-from ethon.telefunc import fast_download, fast_upload
-from ethon.pyfunc import video_metadata
-from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
-from LOCAL.utils import ffmpeg_progress
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from telethon.tl.types import DocumentAttributeVideo
+from ethon.telefunc import fast_download, fast_upload
+from ethon.pyfunc import video_metadata
+
+from .. import Drone, BOT_UN, LOG_CHANNEL
+
+from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
+from LOCAL.utils import ffmpeg_progress
 from main.plugins.actions import LOG_START, LOG_END
 
 async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
