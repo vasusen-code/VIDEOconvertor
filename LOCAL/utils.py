1fd6d5451f328edcbcc7df6a6cbcacc3c75a9ee6
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-05-19 23:22:07 +0530

    Update utils.py

--- LOCAL/utils.py
+++ LOCAL/utils.py
@@ -79,3 +79,33 @@ async def ffmpeg_progress(cmd, file, progress, now, event, ps_name):
                 eta = time_formatter(some_eta)
                 await event.edit(f'{ps_name}\n\n{progress_str}' + f'GROSS: {e_size}\n\nETA: {eta}')
                             
+async def ffmpeg_exec_progress(cmd, file, progress, now, event, ps_name):
+    total_frames = tf(file)
+    with open(progress, "w") as fk:
+        pass
+    proce = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    while proce.returncode != 0:
+        await asyncio.sleep(3)
+        with open(progress, "r+") as fil:
+            text = fil.read()
+            frames = re.findall("frame=(\\d+)", text)
+            size = re.findall("total_size=(\\d+)", text)
+            speed = 0
+            if len(frames):
+                elapse = int(frames[-1])
+            if len(size):
+                size = int(size[-1])
+                per = elapse * 100 / int(total_frames)
+                time_diff = time.time() - int(now)
+                speed = round(elapse / time_diff, 2)
+            if int(speed) != 0:
+                some_eta = ((int(total_frames) - elapse) / speed) * 1000
+                progress_str = "**[{0}{1}]** `| {2}%\n\n`".format(
+                    "".join("█" for i in range(math.floor(per / 5))),
+                    "".join("" for i in range(20 - math.floor(per / 5))),
+                    round(per, 2),
+                )
+                e_size = humanbytes(size) + " of ~" + humanbytes((size / per) * 100)
+                eta = time_formatter(some_eta)
+                await event.edit(f'{ps_name}\n\n{progress_str}' + f'GROSS: {e_size}\n\nETA: {eta}')
+                            
